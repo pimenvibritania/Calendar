4b235012fad4cfee612f1b8fe522ffcfb9d58b36
pimenvibritania <blackbrit13@gmail.com>
2018-03-04 14:59:48 +0700

    part#2 -  (views and template)

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("its work")
+    return render(request, 'myapp/index.html')
 
 # Create your views here.
