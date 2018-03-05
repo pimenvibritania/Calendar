56f1dd1f712193799577054ada4d1873c3b150e4
pimenvibritania <blackbrit13@gmail.com>
2018-03-06 00:35:01 +0700

    part#5 - (forms) 1/2

--- calendarproject/urls.py
+++ calendarproject/urls.py
@@ -5,5 +5,6 @@ from myapp import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('entry/<int:pk>', views.details, name='details'),
+    path('entry/add', views.add, name='add'),
     path('admin/', admin.site.urls),
 ]
